19e4a1d213f3ee9f397344e0d2575462ddd32626
writemorecode <gustav.ae.karlsson@gmail.com>
2017-04-24 23:23:18 +0200

    Removed unused code in problem12.py

--- src/problem12.py
+++ src/problem12.py
@@ -1,14 +1,17 @@
 # Project Euler #12
+
 import math
-import time
 
 def triangle(n):
+    """ Computes the nth triangle number. """
     return n * (n + 1) // 2
 
 def perfect_square(n):
+    """ Checks if n is a perfect square. """
     return (round(math.sqrt(n)) ** 2) == n
 
 def factor_count(n):
+    """ Computes the number of factors of n. """
     factors = 0
 
     for i in range(1, round(math.sqrt(n) + 1)):
@@ -22,21 +25,11 @@ def factor_count(n):
 
     return factors 
 
-def factor2(n):
-    even = lambda n: n % 2 == 0
-    if even(n):
-        return factor2(n / 2) * factor2(n + 1)
-    elif even(n + 1):
-        return factor2(n) * factor2((n + 1) / 2)
-
 n = 1
-start = time.time()
 while True:
     number = triangle(n)
     n += 1
     factors = factor_count(number)
     if factors > 500:
-        end = time.time()
         print(number)
-        print("Elapsed time: {}".format(end - start))
         break
